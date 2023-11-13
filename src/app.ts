82202c001d27fa46fc3d9998840e50835c20d1d1
Cydnirn <seno@siotics.org>
2023-11-14 00:14:56 +0700

    feat: Add listener

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,26 @@
 import express, { Application } from "express";
-import http from "http";
 import env from "#config/index";
 
 import loader from "./loaders";
+import { logger } from "#utils/Logger";
 
 const app = express();
 
-const ExecuteServer = (server: http.Server, port?: number) => {
-    server.listen(env.PORT || port, () => {
-        console.log(`Server is running on port ${env.PORT || port}`);
-    });
+const ExecuteServer = (app: Application, port?: number) => {
+    app.listen(port, () => logger.info(`Server running on port ${port}`));
 };
 
 const StartServer = () => {
     (async (app: Application) => {
         await loader(app);
         console.log(env.ORIGIN);
-        const server = http.createServer(app);
         if (env.NODE_ENV === "production") {
-            ExecuteServer(server, 3002);
+            ExecuteServer(app, 3002);
         } else if (
             env.NODE_ENV === "development" ||
             env.NODE_ENV === "testing"
         ) {
-            ExecuteServer(server, 3001);
+            ExecuteServer(app, 3001);
         }
     })(app);
 };
